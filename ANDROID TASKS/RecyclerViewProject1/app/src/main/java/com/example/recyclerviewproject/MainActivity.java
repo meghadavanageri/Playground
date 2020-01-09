e32e8bf712db6e317435de5c68b01b9e3f03259c
Megha Davanageri <meghadavanageri79@gmail.com>
2020-01-09 15:45:27 +0530

    Assignment_CardView_Completed

---
+++ ANDROID TASKS/RecyclerViewProject1/app/src/main/java/com/example/recyclerviewproject/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.recyclerviewproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private List<ListItem> listItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        listItems = new ArrayList<>();
+
+    for (int i = 0; i<=10; i++){
+        ListItem listItem = new ListItem(
+                "Luke Ray" + (i+1),
+                "Lorem ipsum doler sit amet."
+        );
+        listItems.add(listItem);
+    }
+    adapter = new MyAdapter(listItems,this);
+
+    recyclerView.setAdapter(adapter);
+
+    }
+}
